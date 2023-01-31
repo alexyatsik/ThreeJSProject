39140a58b8c573d04a4b424782879b53f85c79fd
Alexeii <alexeii.yatsenko@gmail.com>
2023-01-31 21:33:21 +0200

    save state

--- src/World/system/renderer.js
+++ src/World/system/renderer.js
@@ -1,7 +1,9 @@
 import {WebGLRenderer} from "three";
 
 export function createRenderer() {
-  const renderer = new WebGLRenderer({antialias: true});
+  const renderer = new WebGLRenderer(
+    {antialias: true}
+  );
   renderer.physicallyCorrectLights = true;
 
   return renderer;
