3f79b0f008b01ac4c918fee441a38a258d7f0629
Alexeii Yatsenko <alexeii.yatsenko@gmail.com>
2023-02-04 14:46:58 +0200

    save state

--- src/index.js
+++ src/index.js
@@ -10,9 +10,8 @@ async function main() {
     const root = new BaseElement()
         .addClass('root')
         .appendTo(document.body);
-    new BaseElement()
+    const contentWrapper = new BaseElement()
         .addClass('content-wrapper')
-        .innerHtml('Lorem ipsum')
         .appendTo(root.DOMElement);
     const canvasWrapper = new BaseElement()
         .addClass('canvas-wrapper')
@@ -22,9 +21,30 @@ async function main() {
     await world.init();
     world.start();
 
-    createUI();
+    createUI(contentWrapper.DOMElement);
 }
 
-function createUI() {
+function createUI(rootElement) {
+    const maxWidthWrapper = new BaseElement()
+        .addClass('content__max-width-wrapper')
+        .appendTo(rootElement)
+        .DOMElement;
 
+    const titleText = 'Challenging the standard of space exploration';
+    new BaseElement('h1')
+        .addClass('content__title-text')
+        .innerHtml(titleText)
+        .appendTo(maxWidthWrapper);
+
+    const contentText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales ligula sit amet semper consectetur.';
+    new BaseElement('p')
+        .addClass('content__text')
+        .innerHtml(contentText)
+        .appendTo(maxWidthWrapper);
+
+    const button = new BaseElement('a')
+        .addClass('button', 'content__button')
+        .innerHtml('Learn More')
+        .appendTo(maxWidthWrapper);
+    button.DOMElement.href = '';
 }
