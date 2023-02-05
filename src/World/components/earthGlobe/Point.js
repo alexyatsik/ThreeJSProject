3dde1366bcb59d7520fa18a864bbb8e7967e6b35
Alexeii Yatsenko <alexeii.yatsenko@gmail.com>
2023-02-05 10:58:56 +0200

    Add animation to the points

--- src/World/components/earthGlobe/Point.js
+++ src/World/components/earthGlobe/Point.js
@@ -1,35 +1,66 @@
 import {BoxGeometry, Matrix4, Mesh, MeshBasicMaterial, SphereGeometry} from "three";
 import {degToRad} from "three/src/math/MathUtils";
+import {gsap} from 'gsap';
 
 export class Point {
     #point
+    #boxGeometryParams = {
+        x: 0.1,
+        y: 0.1,
+        z: 0.8
+    }
+    #radius
+    #lookAtPoint
+    #latitude
+    #longitude
     constructor(latitude, longitude, options) {
-        const {radius, lookAtPoint} = options;
-        if (latitude === undefined || longitude === undefined || radius === undefined)
+        if (latitude === undefined || longitude === undefined || options.radius === undefined)
             throw new Error('Latitude, longitude or radius is not set');
 
-        const x = radius * Math.cos(degToRad(latitude)) * Math.sin(degToRad(longitude));
-        const y = radius * Math.sin(degToRad(latitude));
-        const z = radius * Math.cos(degToRad(latitude)) * Math.cos(degToRad(longitude));
+        this.#radius = options.radius;
+        this.#lookAtPoint = options?.lookAtPoint || null;
+        this.#latitude = latitude;
+        this.#longitude = longitude;
 
-        const boxGeometryParams = {
-            x: 0.1,
-            y: 0.1,
-            z: 0.8
-        }
-        const geometry = new BoxGeometry(boxGeometryParams.x, boxGeometryParams.y, boxGeometryParams.z);
-        const material = new MeshBasicMaterial({
-            color: '#ff0000'
-        });
-        this.#point = new Mesh(geometry, material);
-        this.#point.position.set(x, y, z);
-        this.#point.lookAt(lookAtPoint);
-        this.#point.geometry.applyMatrix4(
-            new Matrix4().makeTranslation(0, 0, -boxGeometryParams.z/2)
-        );
+        this.#build();
+        this.#applyAnimation();
     }
 
     get mesh() {
         return this.#point;
+    }
+
+    #build() {
+        const x = this.#radius * Math.cos(degToRad(this.#latitude)) * Math.sin(degToRad(this.#longitude));
+        const y = this.#radius * Math.sin(degToRad(this.#latitude));
+        const z = this.#radius * Math.cos(degToRad(this.#latitude)) * Math.cos(degToRad(this.#longitude));
+
+        const geometry = new BoxGeometry(
+            this.#boxGeometryParams.x,
+            this.#boxGeometryParams.y,
+            this.#boxGeometryParams.z
+        );
+        const material = new MeshBasicMaterial({
+            color: '#3bf7ff'
+        });
+
+        this.#point = new Mesh(geometry, material);
+        this.#point.position.set(x, y, z);
+        if (this.#lookAtPoint)
+            this.#point.lookAt(this.#lookAtPoint);
+        this.#point.geometry.applyMatrix4(
+            new Matrix4().makeTranslation(0, 0, -this.#boxGeometryParams.z/2)
+        );
+    }
+
+    #applyAnimation() {
+        gsap.to(this.#point.scale, {
+            z: 0,
+            duration: 2,
+            yoyo: true,
+            repeat: -1,
+            delay: Math.random(),
+            ease: 'linear'
+        });
     }
 }
